e993c40784c80a70fac4e2a4848d6be512c17587
inferno-40 <91843738+inferno-40@users.noreply.github.com>
2022-03-28 04:53:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+
+const app =express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'))
+app.set('view engine','ejs');
+
+
+let options ={
+    weekday: 'long', month: 'long', day: 'numeric'
+};
+let day = new Date().toLocaleDateString('en-US',options);
+
+let items = ["Eat","Sleep"];
+
+app.get('/',(req,res)=>{
+    res.render('list',{day: day,items: items});
+});
+
+
+app.post('/',(req,res)=>{
+    items.push(req.body.newItem);
+    res.redirect('/');
+});
+
+app.listen('3000',()=>{
+    console.log('started');
+});
